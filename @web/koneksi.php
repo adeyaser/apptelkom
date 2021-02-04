f18d87656f8d8cb343e49807e0356e6f9100469a
adeyaser <59902463+adeyaser@users.noreply.github.com>
2021-02-04 19:36:25 +0700

    v1
    v1

--- @web/koneksi.php
+++ @web/koneksi.php
@@ -4,4 +4,8 @@
  define('PASS','');
  define('DB','dbtelkom');
  $koneksi = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+
+$date1=date('Y-m-d');
+$orgDate = '10-02-2021';   
+$date2 = date('Y-m-d', strtotime($orgDate)); 
 ?>
